76c1804b1341e059bdd2eb61cae3e40ff46c04eb
Bastiaan Zapf <bzapf@online.de>
2011-04-27 17:13:19 +0200

    tagsordnung-test

---
+++ toys/tagesordnung.php
@@ -0,0 +1,61 @@
+<?
+
+$result= file_get_contents("../cache/marburg/neu/to010.asp.htm");
+//echo $result;
+
+$config = array(
+           'indent'         => true,
+           'output-xhtml'   => true,
+           'wrap'           => 200);
+
+$tidy = new tidy;
+$tidy->parseString($result, $config, 'utf8'); // XXX charset?
+$tidy->cleanRepair();
+
+$doc = new DOMDocument();
+$doc->loadHTML($tidy);
+
+function map_to_children($accum,$function,$node) {
+  $accum=$function($accum,$node);
+  if ($node->childNodes) {
+    foreach ($node->childNodes as $c) {
+      $accum=map_to_children($accum,$function,$c);
+    }
+  }
+  return $accum;
+}
+
+function find_class_zl1_1_2($accum,$node) {
+  if (get_class($node)=="DOMElement") {
+    if (preg_match('/^zl1[12]$/',$node->getAttribute("class"))) {
+      $accum[]=$node;
+      return $accum;
+    }
+  }
+  return $accum;
+}
+
+function accumtext($accum,$node) {
+  if (get_class($node)=="DOMText") {
+    return $accum . trim($node->C14N());
+  }
+  return $accum;
+}
+
+function outputtop($node) {
+  //  var_dump(get_class_methods($node->childNodes->item(0)));
+  //  var_dump($node->childNodes->item(0)->C14N());
+  echo map_to_children('','accumtext',$node);
+  echo "\n***\n";
+}
+
+//$forms=$doc->getElementsByTagName('form');
+$tables=$doc->getElementsByTagName('table');
+
+foreach ($tables as $t) {
+  if ($t->getAttribute("class")=="tl1") {
+    $a=map_to_children(array(),'find_class_zl1_1_2',$t);
+    //    var_dump($a);
+    array_map('outputtop',$a);
+  }
+}
